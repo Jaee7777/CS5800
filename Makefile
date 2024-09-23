47d9ff44fe166259f131ba8ee546d4d6e4662cee
Jaee <oh.jaee@northeastern.edu>
2024-09-23 18:21:42 -0400

    hw02 added

--- Makefile
+++ Makefile
@@ -2,5 +2,5 @@
 Q4: 
 	python -B src/cs5800_hw02_q4.py 3 10 1
 	python -B src/cs5800_hw02_q4.py 3 10 2
-	python -B src/cs5800_hw02_q4.py 5 50 2
-	python -B src/cs5800_hw02_q4.py 5 50 10
+	python -B src/cs5800_hw02_q4.py 9 100 2
+	python -B src/cs5800_hw02_q4.py 9 100 10
--- src/cs5800_hw02_q4.py
+++ src/cs5800_hw02_q4.py
@@ -14,6 +14,7 @@ args = parser.parse_args()
 rnd = np.random.RandomState(10)  # deterministic random data.
 A = rnd.randint(args.max, size=args.N)  # create array A with integer values.
 B = args.t * A  # create an array B = t * A.
+print(f"Array A is {A}, and t is {args.t}")
 
 E = 0  # initialize critical event count.
 i = 0  # initialize indice i count.
@@ -22,7 +23,7 @@ for a_i in A:
     i += 1
     for j in range(i, len(B)):
         if a_i > B[j]:
+            print(f"critical event at a_i = {a_i}, ta_j = {B[j]}")
             E += 1  # add a count to E when critical condition is met.
 
-print(f"Array A is {A}, and t is {args.t}")
 print(f"There are {E} critical events in array A")
